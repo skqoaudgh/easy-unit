329efed76932930d0c1752b681167c716166e468
Bae Myungho <bmh961@naver.com>
2023-05-11 21:56:26 +0900

    Add minify rollup plguin

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs';
 import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
 
 export default {
 	input: 'src/index.js',
@@ -18,5 +19,6 @@ export default {
 		getBabelOutputPlugin({
 			presets: ['@babel/preset-env'],
 		}),
+		terser(),
 	],
 };
